2ee6a57c938b795abac906b78612ae23483380d1
stevengangano <stevengangano@yahoo.com>
2018-03-22 16:20:11 -0700

    Deploy app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-	
-	before_action :set_user, only: [:edit, :update, :show, :destroy] 
-	
+
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
+
 	before_action :require_same_user, only: [:edit, :update, :destroy]
-	
+
 	before_action :require_admin, only: [:destroy]
 
 	#/users
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
-	end	
-	
+	end
+
 	#/signup
 	def new
 		@user = User.new
@@ -18,56 +18,56 @@ class UsersController < ApplicationController
 
 	 #POST   /users
    def create
-     @user = User.new(user_params) 
-     if @user.save 
-			session[:user_id] = @user.id 
-      flash[:success] = "Welcome to the alpha blog #{@user.username}" 
-      redirect_to user_path(@user) 
+     @user = User.new(user_params)
+     if @user.save
+			session[:user_id] = @user.id
+      flash[:success] = "Welcome to the blog #{@user.username}" 
+      redirect_to user_path(@user)
      else
-      render 'new' 
+      render 'new'
      end
    end
 
 	 #/users/:id/edit
    def edit
-#     @user = User.find(params[:id]) 
-   end  
+#     @user = User.find(params[:id])
+   end
 
 	 #/users/:id/
    def update
 #    	@user = User.find(params[:id])
    	if @user.update(user_params)
    	 flash[:success] = "Your account was updated successfully"
-   	 redirect_to articles_path 
+   	 redirect_to articles_path
    	else
    		render :edit
    	end
    end
-	
+
 	#/users/:id
 	def show
-# 		@user = User.find(params[:id]) 
+# 		@user = User.find(params[:id])
 			@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
-	
-	def destroy 
+
+	def destroy
 # 		@user = User.find(param[:id])
 			@user.destroy
 			flash[:danger] = "User and all articles have been deleted"
 			redirect_to users_path
 	end
-	
+
 	private
    def set_user
-	 	@user = User.find(params[:id])	
-	 end  
-  
+	 	@user = User.find(params[:id])
+	 end
+
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password) 
+    params.require(:user).permit(:username, :email, :password)
   end
-	
-	def require_same_user	
+
+	def require_same_user
 	  if current_user != @user and !current_user.admin?
 			flash[:danger] = "You can only edit your own account"
 			redirect_to root_path
@@ -80,5 +80,5 @@ class UsersController < ApplicationController
 			redirect_to root_path
 		end
 	end
-		
+
 end
